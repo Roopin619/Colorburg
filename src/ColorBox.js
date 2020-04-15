7040ea572615914184b5227127de5ea0510ab75f
Roopin <bhadriroopin748@gmail.com>
2020-04-15 18:18:20 +0530

    Refactor ColorBox styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,6 +3,66 @@ import { Link } from 'react-router-dom';
 import chroma from 'chroma-js';
 import './ColorBox.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+    ColorBox: {
+        width: "20%",
+        height: props => (props.showingFullPalette ? "25%" : "50%"),
+        margin: "0 auto",
+        display: "inline-block",
+        position: "relative",
+        cursor: "pointer",
+        marginBottom: "-3px",
+        "&:hover button": {
+            opacity: 1
+        }
+    },
+    copyText: {
+        color: props =>
+            chroma(props.background).luminance() >= 0.7 ? "black" : "white"
+    },
+    colorName: {
+        color: props =>
+            chroma(props.background).luminance() <= 0.1 ? "white" : "black"
+    },
+    seeMore: {
+        color: props =>
+            chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
+        background: "rgba(255, 255, 255, 0.3)",
+        position: "absolute",
+        border: "none",
+        right: "0px",
+        bottom: "0px",
+        width: "3.2rem",
+        height: "1.5rem",
+        textAlign: "center",
+        lineHeight: "1.5rem",
+        textTransform: "uppercase",
+        fontSize: "0.8rem"
+    },
+    copyButton: {
+        color: props =>
+            chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
+        width: "5rem",
+        height: "1.6rem",
+        position: "absolute",
+        display: "inline-block",
+        top: "50%",
+        left: "50%",
+        marginLeft: "-2.5rem",
+        marginTop: "-0.8rem",
+        textAlign: "center",
+        outline: "none",
+        background: "rgba(255, 255, 255, 0.3)",
+        fontSize: "0.8rem",
+        lineHeight: "1.6rem",
+        textTransform: "uppercase",
+        border: "none",
+        textDecoration: "none",
+        opacity: 0
+    }
+};
 
 class ColorBox extends Component {
     constructor(props) {
@@ -17,31 +77,29 @@ class ColorBox extends Component {
     }
 
     render() {
-        const { name, background, paletteId, colorId, showLink } = this.props;
+        const { name, background, paletteId, colorId, showingFullPalette, classes } = this.props;
         const { copied } = this.state;
-        const isDarkColor = chroma(background).luminance() <= 0.1;
-        const isLightColor = chroma(background).luminance() >= 0.7;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-                <div style={{ background }} className="ColorBox">
+                <div style={{ background }} className={classes.ColorBox}>
                     <div
                         style={{ background }}
                         className={`copy-overlay ${copied && 'show'}`}
                     />
                     <div className={`copy-msg ${copied && 'show'}`}>
                         <h1>copied!</h1>
-                        <p className={isLightColor && "dark-text"}>{background}</p>
+                        <p className={classes.copyText}>{background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span className={isDarkColor && "light-text"}>{name}</span>
+                            <span className={classes.colorName}>{name}</span>
                         </div>
-                        <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
+                        <button className={classes.copyButton}>Copy</button>
                     </div>
-                    {showLink && (
+                    {showingFullPalette && (
                         <Link to={`/palette/${paletteId}/${colorId}`} onClick={e => e.stopPropagation()}>
-                        {/* stopPropagation will prevent firing any event from parent. (COPY ANIMATION) */}
-                        <span className={`see-more ${isLightColor && "dark-text"}`}>MORE</span>
+                            {/* stopPropagation will prevent firing any event from parent. (COPY ANIMATION) */}
+                            <span className={classes.seeMore}>MORE</span>
                         </Link>
                     )}
                 </div>
@@ -50,4 +108,4 @@ class ColorBox extends Component {
     }
 }
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
