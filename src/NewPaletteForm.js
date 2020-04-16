aabecf7d84c73c91e79b61877f656aa21d04e41c
Roopin <bhadriroopin748@gmail.com>
2020-04-16 15:22:12 +0530

    Connect colorpicker to button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -73,9 +73,14 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
-    state = {
-        open: false
-    };
+    constructor(props) {
+        super(props);
+        this.state = { 
+            open: true, 
+            currentColor: "teal" ,
+            colors: ["purple", "#e21568"]
+        }
+    }
 
     handleDrawerOpen = () => {
         this.setState({ open: true });
@@ -84,6 +89,14 @@ class NewPaletteForm extends Component {
     handleDrawerClose = () => {
         this.setState({ open: false });
     };
+
+    updateCurrentColor = (newColor) => {
+        this.setState({ currentColor: newColor.hex });
+    }
+
+    addNewColor = () => {
+        this.setState({ colors: [...this.state.colors, this.state.currentColor] });
+    }
 
     render() {
         const { classes } = this.props;
@@ -138,8 +151,16 @@ class NewPaletteForm extends Component {
                             Random Color
                     </Button>
                     </div>
-                    <ChromePicker />
-                    <Button variant="contained" color="primary">
+                    <ChromePicker
+                        color={this.state.currentColor}
+                        onChangeComplete={this.updateCurrentColor}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ backgroundColor: this.state.currentColor }}
+                        onClick={this.addNewColor}
+                    >
                         Add Color
                     </Button>
                 </Drawer>
@@ -149,6 +170,11 @@ class NewPaletteForm extends Component {
                     })}
                 >
                     <div className={classes.drawerHeader} />
+                    <ul>
+                        {this.state.colors.map(color => (
+                            <li style={{ backgroundColor: color }}>{color}</li>
+                        ))}
+                    </ul>
                 </main>
             </div>
         );
