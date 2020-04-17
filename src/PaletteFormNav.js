0c5a0ad7874ec4cb184bff95553c59deae4d7ee6
Roopin <bhadriroopin748@gmail.com>
2020-04-17 22:26:41 +0530

    Tweak styles of form

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -9,6 +9,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import Button from '@material-ui/core/Button';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import styles from './styles/PaletteFormNavStyles';
@@ -47,7 +48,7 @@ class PaletteFormNav extends Component {
                             onClick={handleDrawerOpen}
                             className={classNames(classes.menuButton, open && classes.hide)}
                         >
-                            <MenuIcon />
+                            <AddToPhotosIcon />
                         </IconButton>
                         <Typography variant='h6' color='inherit' noWrap>
                             Create A Palette
