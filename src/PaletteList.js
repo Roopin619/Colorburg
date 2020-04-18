d0e47f01539a249767c01a469df9382a585fae3d
Roopin <bhadriroopin748@gmail.com>
2020-04-18 12:43:18 +0530

    Add bg svg to PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -15,7 +15,7 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1>COLORBURG</h1>
+            <h1 className={classes.heading}>Colorburg</h1>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
