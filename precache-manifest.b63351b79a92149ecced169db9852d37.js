3db95269a06c6762fe49b2e4195791250f5b8c0e
Roopin <bhadriroopin748@gmail.com>
2020-04-19 18:55:55 +0530

    Updates

---
+++ precache-manifest.b63351b79a92149ecced169db9852d37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eeedd1d190726a1e117027bbf059362",
+    "url": "/Colorburg/index.html"
+  },
+  {
+    "revision": "e0c5535a6044c9508772",
+    "url": "/Colorburg/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "86ae76700e44da39621a",
+    "url": "/Colorburg/static/css/main.0f7a9115.chunk.css"
+  },
+  {
+    "revision": "e0c5535a6044c9508772",
+    "url": "/Colorburg/static/js/2.40bfc3dc.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/Colorburg/static/js/2.40bfc3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ae76700e44da39621a",
+    "url": "/Colorburg/static/js/main.9fbc475b.chunk.js"
+  },
+  {
+    "revision": "5d51959a8a6cbbecbf5f",
+    "url": "/Colorburg/static/js/runtime-main.c745500a.js"
+  },
+  {
+    "revision": "926361b76b106da48ede051a584a386b",
+    "url": "/Colorburg/static/media/bg.926361b7.svg"
+  }
+]);
